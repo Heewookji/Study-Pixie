8ef7bb1f52607c34f222230377b49960d35d1f9e
heewookji <wlgmldnr@daum.net>
2019-11-18 23:22:33 +0900

    add marker

--- src/app/pixies/discover/discover.page.ts
+++ src/app/pixies/discover/discover.page.ts
@@ -1,13 +1,5 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-  ViewChild,
-  ElementRef,
-  Renderer2
-} from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { environment } from "../../../environments/environment";
-import * as L from "leaflet";
 import * as mapboxgl from "mapbox-gl";
 
 @Component({
@@ -15,43 +7,101 @@ import * as mapboxgl from "mapbox-gl";
   templateUrl: "./discover.page.html",
   styleUrls: ["./discover.page.scss"]
 })
-export class DiscoverPage implements OnInit, AfterViewInit {
-  private map;
-  private style = "mapbox://styles/mapbox/streets-v11";
-  constructor(private renderer: Renderer2) {}
+export class DiscoverPage implements OnInit {
+  map: mapboxgl.Map;
+  // style = "mapbox://styles/devserv/ck34b1p5i1bpp1cl32nylbsw7";
+  style = "mapbox://styles/mapbox/light-v10";
+  lat = 37.75;
+  lng = -122.41;
+  init = false;
+  monument: mapboxgl.LngLatLike = [-122.414, 37.776];
+
+  constructor() {}
 
   ngOnInit() {}
 
-  // leaflet
-  private initMap(): void {
-    this.map = L.map("map", {
-      center: [39.8282, -98.5795],
-      zoom: 3
+  //ionview에 진입하고 맵을 출력한다.
+  ionViewDidEnter() {
+    if (!this.init) {
+      mapboxgl.accessToken = environment.mapboxAccessToken;
+      this.map = new mapboxgl.Map({
+        container: "map",
+        style: this.style,
+        zoom: 13,
+        center: this.monument
+      });
+    }
+    this.init = true;
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+    this.map.on("load", event => {
+      this.map.addLayer({
+        id: "points",
+        type: "symbol",
+        source: {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: [
+              {
+                // feature for Mapbox DC
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [-77.03238901390978, 38.913188059745586]
+                },
+                properties: {
+                  title: "Mapbox DC",
+                  icon: "monument"
+                }
+              },
+              {
+                // feature for Mapbox SF
+                type: "Feature",
+                geometry: {
+                  type: "Point",
+                  coordinates: [-122.414, 37.776]
+                },
+                properties: {
+                  title: "Mapbox SF",
+                  icon: "harbor"
+                }
+              }
+            ]
+          }
+        },
+        layout: {
+          // get the icon name from the source's "icon" property
+          // concatenate the name to get an icon from the style's sprite sheet
+          "icon-image": "marker-15",
+          // get the title name from the source's "title" property
+          "text-field": ["get", "title"],
+          "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+          "text-offset": [0, 0.6],
+          "text-anchor": "top"
+        }
+      });
+
+      // create the popup
+      var popup = new mapboxgl.Popup({ offset: 25 }).setText(
+        "Construction on the Washington Monument began in 1848."
+      );
+
+      // create DOM element for the marker
+      var el = document.createElement("div");
+      el.className = "marker";
+      el.style.backgroundImage =
+        "url(https://docs.mapbox.com/mapbox-gl-js/assets/washington-monument.jpg)";
+      el.style.width = "50px";
+      el.style.height = "50px";
+      el.style.borderRadius = "50%";
+
+      // create the marker
+      new mapboxgl.Marker(el)
+        .setLngLat(this.monument)
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(this.map);
     });
-  }
-
-  //view 단이 출력 된 뒤 수행하는 메서드
-  ngAfterViewInit() {
-    //leaflet 이용할 경우
-    this.initMap();
-    const tiles = L.tileLayer(
-      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      {
-        maxZoom: 19,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-      }
-    );
-   tiles.addTo(this.map);
-
-    //mapbox 이용할 경우
-    // this.map = new mapboxgl.Map({
-    //   container: "map",
-    //   style: this.style,
-    //   zoom: 13,
-    //   center: [39.8282, -98.5795]
-    // });
-    // // Add map controls
-    // this.map.addControl(new mapboxgl.NavigationControl());
   }
 }
