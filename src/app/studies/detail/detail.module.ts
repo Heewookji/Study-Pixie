c0c0e85ed817f5f04bce25151eeeb239921da59b
heewookji <wlgmldnr@daum.net>
2020-01-29 22:27:59 +0900

    detail study location

--- src/app/studies/detail/detail.module.ts
+++ src/app/studies/detail/detail.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { DetailPage } from './detail.page';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   {
