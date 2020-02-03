e7c95dd7a75126590d2c69e1986a0cbc42ca1d59
heewookji <wlgmldnr@daum.net>
2020-02-03 21:22:25 +0900

    register

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "./auth.service";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
+import { ActionSheetController } from "@ionic/angular";
 
 @Component({
   selector: "app-auth",
@@ -9,16 +10,48 @@ import { Router } from '@angular/router';
   styleUrls: ["./auth.page.scss"]
 })
 export class AuthPage implements OnInit {
-  isLogin = true;
-
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private actionSheetCtrl: ActionSheetController
+  ) {}
 
   ngOnInit() {}
 
-  onSwitchAuthMode() {
-    this.isLogin = !this.isLogin;
+  signUp() {
+    this.actionSheetCtrl.create({
+      header: "회원가입",
+      buttons: [
+        {
+          text: "일반 회원가입",
+          handler: () => {
+            console.log("일반 clicked");
+          }
+        },
+        {
+          text: "구글 아이디로 가입",
+          icon: "logo-google",
+          handler: () => {
+            console.log("구글 clicked");
+          }
+        },
+        {
+          text: "페이스북 아이디로 가입",
+          icon: "logo-facebook",
+          handler: () => {
+            console.log("페이스북 clicked");
+          }
+        },
+        {
+          text: "취소",
+          role: "cancel"
+        }
+      ]
+    }).then( actionSheetEl => {
+      actionSheetEl.present();
+    })
   }
-  
+
   onSubmit(form: NgForm) {
     if (!form.valid) {
       return;
@@ -30,6 +63,6 @@ export class AuthPage implements OnInit {
   }
   authenticate(email: string, password: string) {
     this.authService.login(email, password);
-    this.router.navigateByUrl("pixies/tabs/discover");
+    this.router.navigateByUrl("studies/tabs/discover");
   }
 }
